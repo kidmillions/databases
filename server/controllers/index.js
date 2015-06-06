6dc55905efc7b4c3f150bd190a25eb115d6c7e2d
kidmillions <claymanchris@gmail.com>
2015-06-06 16:53:10 -0700

    added sequelize and moved posts and gets into controller

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,34 +1,97 @@
 var models = require('../models');
 var bluebird = require('bluebird');
+var db = require('../db');
+
+//db.users
+//db.messages
+//db.rooms
 
 
 module.exports = {
   messages: {
     get: function (req, res) {
       // console.log(req);
+      db.messages.findAll()
+
+
+
+
       models.messages.get(function(response) {
         // console.log(response);
         res.json(response);
       });
     }, // a function which handles a get request for all messages
     post: function (req, res) {
-      models.messages.post(req.body);
-      res.send(req.body);
+      db.users.findOrCreate({
+        where: {
+          name: req.body.username
+        }
+      })
+      .then(function(user_id) {
+        db.rooms.findOrCreate({
+          where: {
+            name: req.body.roomname
+          }
+        }).then(function(room_id) {
+
+          return [user_id, room_id];
+        })
+        .then(function(result) {
+          console.log(result[0][0].dataValues.id);
+          console.log(result[1][0].dataValues.id);
+          db.messages.create({
+            text: req.body.text,
+            userId: result[0][0].dataValues.id,
+            roomId: result[1][0].dataValues.id
+          })
+          .then(function(result) {
+            res.sendStatus(201);
+          })
+        })
+      }).catch(function(err) {
+        console.log('error! ', err);
+      });
+
+
+
+
+      // .then(function(result) {
+      //   db.messages.create()
+      // })
+      // models.messages.post(req.body);
+      // res.send(req.body);
     } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
     get: function (req, res) {
-      models.users.get(function(response) {
-        res.send(response);
-      });
+      db.users.findAll()
+        .then(function(results) {
+          res.json(results);
+        })
+        .catch(function(err) {
+          console.log("Error: ", err);
+        });
+
+
+      // models.users.get(function(response) {
+      //   res.send(response);
+      // });
     },
     post: function (req, res) {
       // console.log(req.body);
-      models.users.post(req.body, function(results) {
-       res.send(results);
-      });
+      db.users.create(req.body)
+        .then(function(results) {
+          res.sendStatus(201);
+        })
+
+
+
+
+      // models.users.post(req.body, function(results) {
+      //  res.send(results);
+      // });
     }
   }
 };
--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,81 @@
 var sqlite3 = require('sqlite3');
 var path = require('path');
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('chat', '', '', {
+  storage: '/Users/HR10/Desktop/databases/chat',
+  dialect: 'sqlite',
+});
 
-module.exports = new sqlite3.Database(path.join(__dirname, '../../chat'));
+var Message = sequelize.define('message', {
+  text: Sequelize.STRING
+});
+
+var User = sequelize.define('user', {
+  name: Sequelize.STRING
+});
+
+var Room = sequelize.define('room', {
+  name: Sequelize.STRING
+});
+
+User.hasMany(Message);
+
+Room.hasMany(Message);
+
+Message.belongsTo(User);
+Message.belongsTo(Room);
+
+Message.sync();
+Room.sync();
+User.sync();
+
+module.exports.messages = Message;
+module.exports.rooms = Room;
+module.exports.users = User;
+
+// User.create({'name': 'Eric'})
+//   .then(function(err, result) {
+//     Room.create({'name': 'Default'})
+//     .then(function(err, result) {
+//       User.findOne({where: {name: 'Eric'}})
+//       .then(function(result) {
+//         console.log(result.dataValues);
+//       });
+//     });
+//   })
+//   .catch(function(err) {
+//     console.log("No chance this gets run...", err.message);
+//   });
+
+//     ))
+
+//   .then(function(err, result) {
+//     Message.create({userId: result.id, 'text': 'Hello World'})
+//   })
+
+// Message.findAll().then(function(results) {console.log(results)});
+
+// module.exports = new sqlite3.Database(path.join(__dirname, '../../chat'));
 
 
+
+
+// var path = require('path');
+// var Sequelize = require("sequelize");
+// var sequelize = new Sequelize("zoo", "", "", {
+//   dialect: 'sqlite',
+//   storage: '../zoo.sqlite'
+// });
+
+// var Animal = sequelize.define('Animal', {
+//   name: Sequelize.STRING,
+//   age: Sequelize.INTEGER
+// });
+
+// Animal.sync({force: true})
+// .then(function() {
+//   return Animal.create({
+//     name: "Bobcat",
+//     age: 5
+//   });
+// });
